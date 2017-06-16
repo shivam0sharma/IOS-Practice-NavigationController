ab95b19d10751b49687566618fbf3b59c1f15bbb
Shaisan2 <shivam0sharma@icloud.com>
2017-06-16 12:20:46 -0400

    Added the navigation controllers

---
+++ Navigation-Controller/YellowVC.swift
@@ -0,0 +1,38 @@
+//
+//  YellowVC.swift
+//  Navigation-Controller
+//
+//  Created by Shivam Sharma on 6/16/17.
+//  Copyright © 2017 ShivamSharma. All rights reserved.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func popToRed(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
